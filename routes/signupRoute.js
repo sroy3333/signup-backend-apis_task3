f1ffacc50bbc19dfc66166db1ad259c84c2c973f
sroy3333 <121767970+sroy3333@users.noreply.github.com>
2024-04-01 02:19:43 +0530

    Update and rename signupRoute.js to user.js

--- routes/signupRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const signupController = require('../controllers/signupController');
-
-// POST route for signup
-router.post('/user/signup', signupController);
-
-module.exports = router;
