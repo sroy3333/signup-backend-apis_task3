02667de15a1acb4ae78f14be158a9f530f0dec8d
sroy3333 <121767970+sroy3333@users.noreply.github.com>
2024-04-01 02:22:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,34 @@
 const path = require('path');
 
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const sequelize = require('./util/database');
-const signupRoute = require('./routes/signupRoute');
 
-const signup = require('./models/signup');
+const userRoutes = require('./routes/user');
+
+const User = require('./models/users');
 
 var cors = require('cors');
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
 app.use(cors());
 
-app.signupRoute = require('./models/signup');
+app.use(express.json());
 
-// Middleware setup
-app.use(bodyParser.json({ extended: false }));
+app.use('/user', userRoutes);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
-app.use('/user/signup', signupRoute);
-
-// Route for serving the signup form
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'signup.html'));
 });
 
-// Start the server
 sequelize
 .sync()
-.then(result => {
+.then(() => {
     app.listen(5000);
 })
-.catch(error => {
-    console.log(error);
+.catch(err => {
+    console.log(err);
 });
